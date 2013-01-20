cd2917650abb7deef8b4ea362b322bd3eff6c25c
dieesrod <dieesrod@gmail.com>
2013-01-20 18:09:01 +0100

    Now it is working !!!

--- cat_and_mouse/src/node-base.lisp
+++ cat_and_mouse/src/node-base.lisp
@@ -7,16 +7,11 @@
 ;;; ---------------------------------------------------------------------------
 
 (defvar *mouse-pose* (make-fluent :name :mouse-pose) "current pose of mouse")
-(defvar *mouse-pose-sub* nil "pose subscription client")
+(defvar *mouse-pose-sub* nil "mouse pose subscription client")
 
 (defvar *cat-pose* (make-fluent :name :cat-pose) "current pose of cat")
-(defvar *cat-pose-sub* nil "pose subscription client")
-(defvar *cat-cmd-vel-pub* nil "command velocity subscription client")
-
-(defvar *dbg-mouse-pose* (make-fluent :name :dbg-mouse-pose) "current pose of cat")
-
-(defvar *mouse-pose-pub* nil "command velocity subscription client")
-(defvar *cat-pose-pub* nil "command velocity subscription client")
+(defvar *cat-pose-sub* nil "cat pose subscription client")
+(defvar *cat-cmd-vel-pub* nil "cat command velocity subscription client")
 
 ;;; ---------------------------------------------------------------------------
 
@@ -39,84 +34,85 @@
                         #'cat-pose-cb))  
   (setf *cat-cmd-vel-pub* (advertise "CAT/Motion_Controller.001"
                            "geometry_msgs/Twist"))
-    
-  ;;(setf *mouse-pose-pub* (advertise "MOUSE/TurtlePose"
-  ;;                         "geometry_msgs/Twist"))
-  ;;(setf *cat-pose-pub* (advertise "CAT/TurtlePose"
-  ;;                         "geometry_msgs/Twist"))
 )
 
 (defun mouse-pose-cb (msg)
-  "Callback for pose values"
+  "Callback for mouse pose values"
   (setf (value *mouse-pose*) 
     (turlesim-msg-from-nav-msg msg))
-  ;;
-  (setf (value *dbg-mouse-pose*) msg)
-  ;;(publish *mouse-pose-pub* (turlesim-msg-from-nav-msg msg))
 )
 
 (defun cat-pose-cb (msg)
   "Callback for cat pose values"
   (setf (value *cat-pose*) 
     (turlesim-msg-from-nav-msg msg))
-  ;;
-  ;;(publish *cat-pose-pub* (value *cat-pose*))
 )
 
 (defun create-vel-cmd (lin ang) 
-  "function to send velocity commands"
+  "Creates a geometry message to be used by a velocity command."
   (roslisp:make-message "geometry_msgs/Twist" 
     :linear  (make-message "geometry_msgs/vector3" (x) lin)
     :angular (make-message "geometry_msgs/vector3" (z) ang))
 )
 
 (defun send-vel-cmd (lin ang)
-   "function to send velocity commands"
-   (setf msg (create-vel-cmd lin ang))
-   (publish *cat-cmd-vel-pub* msg)
+   "Function to send velocity commands through ROS topic."
+   (let ((msg nil)) 
+     (setf msg (create-vel-cmd lin ang))
+     (publish *cat-cmd-vel-pub* msg)
+   )
 )
 
 ;;; ---------------------------------------------------------------------------
 
 (defun parse-nav-message (msg) 
-  (roslisp:with-fields (pose) msg
-    (setf tmp1 pose) )
-  (roslisp:with-fields (pose) tmp1
-    (setf tmp2 pose) )
-  (roslisp:with-fields (position orientation) tmp2
-    (setf ori orientation)
-    (setf pos position) )
-  (list pos ori)
+  "Extracts position and orientation vectors from a navigation message." 
+  (let ((tmp1 nil) (tmp2 nil) (pos nil) (ori nil)) 
+    (roslisp:with-fields (pose) msg
+      (setf tmp1 pose) )
+    (roslisp:with-fields (pose) tmp1
+      (setf tmp2 pose) )
+    (roslisp:with-fields (position orientation) tmp2
+      (setf ori orientation)
+      (setf pos position) )
+    (list pos ori)
+  )
 )
 
-(defun create-turlesim-msg (x y rot) 
+(defun create-turlesim-msg (x y rot)
+  "Create a turtlesim-msg using X, Y and rotation angle." 
   (make-message "turtlesim/Pose" :x x :y y :theta rot)
 )
 
-(defun turlesim-msg-from-nav-msg (msg) 
-  (setf pose (parse-nav-message msg))
-  (roslisp:with-fields (X Y Z) (first pose)
-    (setf x1 X)
-    (setf y1 Y))  
-  (roslisp:with-fields (X Y Z W) (second pose)
-    (setf q1 (make-quaternion X Y Z W))
-    (setf theta (cl-transforms:get-yaw q1)))  
-  (create-turlesim-msg x1 y1 theta) 
+(defun turlesim-msg-from-nav-msg (msg)
+  "Returns a turtlesim-msg from a navigation message." 
+  (let ((pose nil) (x1 nil) (y1 nil) (q1 nil) (theta nil)) 
+    (setf pose (parse-nav-message msg))
+    (roslisp:with-fields (X Y Z) (first pose)
+      (setf x1 X) 
+      (setf y1 Y))
+    (roslisp:with-fields (X Y Z W) (second pose)
+      (setf q1 (make-quaternion X Y Z W))
+      (setf theta (cl-transforms:get-yaw q1)))
+    (create-turlesim-msg x1 y1 theta)
+  )
 )
 
 ;;; ---------------------------------------------------------------------------
 
 (defun pose-msg->transform (msg)
-  "returns a transform proxy that allows to transform into the frame given by x, y, and theta of msg."
+  "Returns a transform proxy that allows to transform into the frame given 
+   by x, y, and theta of turtlesim-msg."
   (with-fields (x y theta) msg
     (cl-transforms:make-transform
-     (cl-transforms:make-3d-vector x y 0)
-     (cl-transforms:axis-angle->quaternion
-      (cl-transforms:make-3d-vector 0 0 1)
-      theta))))
+      (cl-transforms:make-3d-vector x y 0)
+      (cl-transforms:axis-angle->quaternion
+        (cl-transforms:make-3d-vector 0 0 1) theta)))
+)
 
 (defun relative-angle-to (goal pose)
-  "Given a pose as 3d-pose msg and a goal as 3d vector, calculate the angle by which the pose has to be turned to point toward the goal."
+  "Given a pose as 3d-pose msg and a goal as 3d vector, calculate the angle 
+   by which the pose has to be turned to point toward the goal."
   (let ((diff-pose (cl-transforms:transform-point
                      (cl-transforms:transform-inv
                        (pose-msg->transform pose))
@@ -127,8 +123,8 @@
 )
 
 (defun calculate-cat-angular-vel (goal &optional (ang-vel-factor 2.0))
-  "Uses the current turtle pose and calculates the angular velocity
-  command to turn towards the goal."
+  "Uses the current cat pose and calculates the angular velocity command to 
+   turn towards the goal."
   (* ang-vel-factor
      (relative-angle-to goal (value *cat-pose*)))
 )
--- cat_and_mouse/src/simple-plan.lisp
+++ cat_and_mouse/src/simple-plan.lisp
@@ -5,6 +5,7 @@
 (in-package :cmg)
 
 (def-cram-function move-to-pos (goal &optional (distance-threshold 1.0))
+  "Plan to move cat to a fixed position defined as 3d-vector"
   (let ((reached-fl (< (fl-funcall #'cl-transforms:v-dist
                                    (fl-funcall
                                        #'cl-transforms:translation
@@ -25,6 +26,7 @@
 )
 
 (def-cram-function go-to (goal &optional (distance-threshold 1.0))
+  "Plan to follow a goal defined as a fluent"
   (let ((reached-fl (< (fl-funcall #'cl-transforms:v-dist
                                    (fl-funcall
                                        #'cl-transforms:translation
@@ -44,51 +46,8 @@
       (send-vel-cmd 0 0)))
 )
 
-(def-cram-function follow-mouse-2 (&optional (distance-threshold 5.0))
-  (let ((reached-fl (< (fl-funcall #'cl-transforms:v-dist  
-                                    (fl-funcall
-                                        #'cl-transforms:translation
-                                        (fl-funcall
-                                            #'pose-msg->transform
-                                            *cat-pose*))
-                                    (fl-funcall
-                                        #'cl-transforms:translation
-                                        (fl-funcall
-                                            #'pose-msg->transform
-                                            *mouse-pose*)) )                                            
-                       distance-threshold)))
-    (unwind-protect
-         (pursue
-           (loop do
-
-             (format t "~a~%" (value *mouse-pose*) )
-             (format t "~a~%" (value *cat-pose*) )
-                                                     
-             (format t "~a~%" (value (fl-funcall #'cl-transforms:v-dist  
-                                    (fl-funcall
-                                        #'cl-transforms:translation
-                                        (fl-funcall
-                                            #'pose-msg->transform
-                                            *cat-pose*))
-                                    (fl-funcall
-                                        #'cl-transforms:translation
-                                        (fl-funcall
-                                            #'pose-msg->transform
-                                            *mouse-pose*)) ))
-             )
-           )
-           
-           (wait-for reached-fl)
-           (loop do
-             (send-vel-cmd
-               1.5
-               (calculate-cat-angular-vel (cl-transforms:translation
-                    (pose-msg->transform (value *mouse-pose*)))))
-             (wait-duration 0.1)))
-      (send-vel-cmd 0 0)))
-)
-
 (def-cram-function follow-mouse (&optional (distance-threshold 5.0))
+  "Plan to follow the mouse"
   (let ((reached-fl (< (fl-funcall #'cl-transforms:v-dist  
                                     (fl-funcall
                                         #'cl-transforms:translation
